471baa7c9849e3eb0e4e69aee926f646d4fd8881
Sathvik Aithala <sathvik@Sathviks-MacBook-Pro.local>
2020-08-05 16:16:52 -0500

    adding Linestrings files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level (4 on a scale of 0-18).
-let map = L.map('mapid').setView([40.7, -94.5], 4); 
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 /* another way to set the map:
 
@@ -17,10 +17,10 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', { //We assign the tileLayer() method, as shown in the Quick Start Guide’s “Setting up the map” section to the variable streets. Leaflet doesn't provide a tile layer. Instead, it offers various tile layer APIs.
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	// id: 'mapbox.streets', //We add the id attribute and assign it mapbox.streets, which will show the streets on the map.
-	accessToken: API_KEY
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // id: 'mapbox.streets', //We add the id attribute and assign it mapbox.streets, which will show the streets on the map.
+    accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map); //Finally, we call the addTo() function with our map object, map on our graymap object tile layer. The addTo() function will add the graymap object tile layer to our let map.
